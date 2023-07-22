860a0d721964388e43a60f7badec09a3038ff984
Juan Carlos Saavedra Serrato <131192308+juancarlossaavedra@users.noreply.github.com>
2023-07-22 10:16:10 -0500

    Delete challenger-alura-geek directory

--- challenger-alura-geek/js/app.js
+++
@@ -1,9 +0,0 @@
-import { valida } from "./validaciones-formularios.js";
-
-const inputs = document.querySelectorAll(".inputs");
-
-inputs.forEach((input) => {
-    input.addEventListener("blur", (input) => {
-        valida(input.target);
-    });
-});
